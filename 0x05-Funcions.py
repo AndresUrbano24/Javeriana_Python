f9e06c5a41cdd23585dce0626f06f11fd022224a
AndresUrbano24 <110997732+AndresUrbano24@users.noreply.github.com>
2022-08-10 11:02:59 -0500

    Add files via upload

---
+++ 0x05-Funcions.py
@@ -0,0 +1,16 @@
+def sumar(suma1,suma2):
+  print("resultado de suma",suma1+suma2)
+sumar(3,8)
+
+
+def restar(resta1,resta2):
+    print("resultado de restar",resta1-resta2)
+restar(5,11)    
+
+def multiplicacion(multiplicando,multiplicador):
+    print("resultado de multiplicacion",multiplicando*multiplicador)
+multiplicacion(8,6)    
+
+def potencia(base,exponente):
+   print("resultado de la potenca",base**exponente)
+potencia(10,2)
